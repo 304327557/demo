f5f620d2fc8ae85f0bca46bfe1782a88a9c4e1b0
project <304327557@qq.com>
2019-01-04 17:01:28 +0800

    logback和log4j

--- src/main/java/com/hao/controller/UserController.java
+++ src/main/java/com/hao/controller/UserController.java
@@ -2,15 +2,20 @@ package com.hao.controller;
 
 
 import com.hao.bean.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/index")
 public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @Value(value = "${hao.secret}")
     private String value;
@@ -19,6 +24,7 @@ public class UserController {
     @RequestMapping("/test")
     public String demoController(){
         System.out.println("hhhhhhhhhhhhh...");
+        LOG.info("log4j-----------------------");
         return "hello world!";
     }
 
